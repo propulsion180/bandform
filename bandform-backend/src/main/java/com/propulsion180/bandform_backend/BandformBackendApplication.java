00b95ca161d664e5a17e4d348cf4b903f46e3871
propulsion180 <wmmperera@tuta.io>
2026-02-03 23:27:28 +1300

    project format setup

---
+++ bandform-backend/src/main/java/com/propulsion180/bandform_backend/BandformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.propulsion180.bandform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandformBackendApplication.class, args);
+	}
+
+}
---
+++ bandform-backend/src/test/java/com/propulsion180/bandform_backend/BandformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propulsion180.bandform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
